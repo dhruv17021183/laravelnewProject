02aeb3ef84c77574770a53b197ad51fae3cb353c
Dhruv <dsuthar1183@gmail.com>
2022-03-25 14:13:21 +0530

    New Commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -171,4 +171,9 @@ class PostController extends Controller
         // return view('posts.edit',['post'=>BlogPost::findOrFail($id)]);
         return view('home');
     }
+    public function home1()
+    {
+        // return view('posts.edit',['post'=>BlogPost::findOrFail($id)]);
+        return view('home');
+    }
 }
